5907a778cd65a942483295905d8a8938ea877d4c
Dave Fearon <dave.fearon@gmail.com>
2011-11-29 13:24:02 -0500

    Some stuff to make the qr code look and work better

--- now/index.php
+++ now/index.php
@@ -1,11 +1,15 @@
 <?php
+
+//var_dump( $_REQUEST['q'] );
+
+
 $PNG_TEMP_DIR = dirname(__FILE__).DIRECTORY_SEPARATOR.'temp'.DIRECTORY_SEPARATOR;
 $PNG_WEB_DIR = 'temp/';
 include "qrlib.php";
 $referer = ( $_SERVER['HTTP_REFERER'] != null ) ? $_SERVER['HTTP_REFERER'] : 'quickqr.me';
 $filename = $PNG_TEMP_DIR.urlencode($referer).'.png';
 $errorCorrectionLevel = 'L';
-$matrixPointSize = 4;
+$matrixPointSize = 2;
 QRcode::png($referer, $filename, $errorCorrectionLevel, $matrixPointSize, 2);
 header( "Content-type: image/png" );
 readfile($PNG_WEB_DIR.basename($filename));
